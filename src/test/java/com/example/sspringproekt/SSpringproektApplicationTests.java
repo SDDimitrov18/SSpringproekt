355d70801875fd79e0c64df10aead58a6bca0b7e
SDDimitrov18 <SDDimitrov18@codingburgas.bg>
2022-10-24 11:45:37 +0300

    Created Spring boot project

---
+++ src/test/java/com/example/sspringproekt/SSpringproektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sspringproekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SSpringproektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
